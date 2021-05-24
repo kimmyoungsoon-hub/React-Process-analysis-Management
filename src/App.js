4c5a0bc490b9962e1028b803c7185f6b29b5a6e3
kimmyoungsoon-hub <75721315+kimmyoungsoon-hub@users.noreply.github.com>
2021-05-24 15:24:17 +0900

    Creat Customers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-       <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>              
-    </div>
-  );
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placimg.com/64/64/1',
+  'name' : '홍길동1',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '회사원'
+},
+{
+  'id' : 2,
+  'image' : 'https://placimg.com/64/64/2',
+  'name' : '홍길동2',
+  'birthday' : '961223',
+  'gender' : '여자',
+  'job' : '회사원'
+},
+{
+  'id' : 3,
+  'image' : 'https://placimg.com/64/64/3',
+  'name' : '홍길동3',
+  'birthday' : '941224',
+  'gender' : '남자',
+  'job' : '변호사'
 }
+]
 
+
+class App extends Component {
+  render () {
+    return (s
+      <div>
+        {
+          customers.map( c =>{
+            return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            );
+          })
+        }
+        
+  </div>
+    );
+
+  }
+}
 export default App;
